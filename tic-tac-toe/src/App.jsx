cac974fcb91b9bc16aba278c23f502b5bfee631b
jadentoon <jaden.toon@gmail.com>
2025-02-04 16:20:53 +0000

    Tic Tac Toe in React Js

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TicTacToe from './Components/TicTacToe/TicTacToe'
+
+function App() {
+
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  )
+}
+
+export default App
